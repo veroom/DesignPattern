74d9cb9f751e2183926c37dd9667879e8ccb7349
博人 水上 <vw.vw.colocolo@gmail.com>
2022-01-11 23:56:50 +0900

    コミットテスト

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -6,7 +6,7 @@ namespace Iterator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("TEST!");
         }
     }
 }
