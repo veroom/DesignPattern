00cde74287a117c2de7f154219a26768e2794da6
博人 水上 <vw.vw.colocolo@gmail.com>
2022-01-16 10:46:15 +0900

    デザインパターン：Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            Singleton obj1 = Singleton.singleton;
+            Singleton obj2 = Singleton.singleton;
+
+            // インスタンスの比較
+            if (obj1 == obj2)
+            {
+                Console.WriteLine("同一インスタンスです");
+            }
+            else
+            {
+                Console.WriteLine("別インスタンスです");
+            }
+
+            Console.WriteLine("End");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Singleton
+{
+    class Singleton
+    {
+        /// <summary>
+        /// シングルトンパターン
+        /// </summary>
+        public static Singleton singleton
+        {
+            get { return m_singleton; }
+        }
+        private static Singleton m_singleton = new Singleton();
+
+        // コンストラクタ
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+    }
+}
