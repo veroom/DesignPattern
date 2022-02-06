2e2f4f9bb33200ff9140ee2a1947b31088a4ef37
博人 水上 <vw.vw.colocolo@gmail.com>
2022-02-06 22:40:58 +0900

    デザインパターン：Prototype、Composite

---
+++ Composite/FileTreatmentException .cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Composite
+{
+    class FileTreatmentException : Exception
+    {
+
+        // コンストラクタ
+        public FileTreatmentException() : base()
+        {
+        }
+
+        // コンストラクタ
+        public FileTreatmentException(string message) : base(message)
+        {
+        }
+
+        // コンストラクタ
+        public FileTreatmentException(string message, Exception innnerException) : base(message, innnerException)
+        {
+        }
+    }
+}
